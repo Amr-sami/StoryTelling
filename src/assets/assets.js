de0b8c2768d1a769673a76fe239b0981c86f29bf
ZiadEssam <zizoessam2015@icloud.com>
2024-07-16 06:21:32 +0300

    Modified5

---
+++ src/assets/assets.js
@@ -0,0 +1,51 @@
+import history_icon from './history_icon.png'
+import menu_icon from './menu_icon.png'
+import plus_icon from './plus_icon.png'
+import question_icon from './question_icon.png'
+import setting_icon from './setting_icon.png'
+import bulb_icon from './bulb_icon.png'
+import compass_icon from './compass_icon.png'
+import gallery_icon from './gallery_icon.png'
+import mic_icon from './mic_icon.png'
+import user_icon from './user_icon.png'
+import youtube_icon from './youtube_icon.png'
+import message_icon from './message_icon.png'
+import code_icon from './code_icon.png'
+import send_icon from './send_icon.png'
+import gemini_icon from './gemini_icon.png'
+import CheckBox from './CheckBox.png'
+import Regenerate from './Regenerate.png'
+import Batman from './batman.jpg'
+import forest from "./forest.png"
+import Ai from "./Ai.png"
+import narrative from "./Narrative.jpeg"
+import save from "./Save_button.jpg"
+import About from "./About.svg"
+import Continue from "./Continue.png"
+
+export const assets = {
+    history_icon,
+    menu_icon,
+    plus_icon,
+    question_icon,
+    setting_icon,
+    bulb_icon,
+    compass_icon,
+    gallery_icon,
+    mic_icon,
+    user_icon,
+    youtube_icon,
+    message_icon,
+    code_icon,
+    send_icon,
+    gemini_icon,
+    CheckBox,
+    Regenerate,
+    Batman,
+    forest,
+    Ai,
+    narrative,
+    save,
+    About,
+    Continue
+}
